cba31a3773203083d9062fbfc2fd39064202a7fd
IvanRyabov89 <138253067+IvanRyabov89@users.noreply.github.com>
2023-09-17 17:59:34 +0300

    Add files via upload

---
+++ Library/IFauna.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public interface IFauna
+    {
+        /// <summary>
+        /// класс
+        /// </summary>
+        string Class { get; set; }
+        /// <summary>
+        /// род
+        /// </summary>
+        string Genus { get; set; }
+        /// <summary>
+        /// порода
+        /// </summary>
+        string Breed { get; set; }
+        /// <summary>
+        /// возраст
+        /// </summary>
+        string Age { get; set; }
+
+    }
+}
---
+++ Library/NewClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class NewClass : IFauna, INotifyPropertyChanged
+    {
+        public string Class { get; set; }
+        public string Genus { get; set; }
+        public string Breed { get; set; }
+        public string Age { get; set; }
+
+        public NewClass()
+        {
+            this.Class = "Not Determined";
+            this.Genus = "Not Determined";
+            this.Breed = "Not Determined";
+            this.Age = "Not Determined";
+        }
+        public override string ToString()
+        {
+            return $"{this.Class}{this.Genus}{this.Breed}{this.Age}";
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
